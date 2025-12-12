c097863e5fe2a0e42c1547fbf40a6497a710dfaa
shubhamthakremongoose <shubham.thakre.mongooseintern@gmail.com>
2025-12-12 11:11:21 +0530

    Fixed deployement error

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindCss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindCss()],
+  plugins: [react(), tailwindCss()],
+  base: '/usdce-project/', 
 })
